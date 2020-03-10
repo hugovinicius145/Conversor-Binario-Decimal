e58218c62198f862f659e5621f389d1581bf80c0
hugovinicius145 <hugovinicius145@gmail.com>
2020-03-10 11:49:08 -0300

    Updates

---
+++ precache-manifest.4e8fa0d661d8b7df2676c5be49bef19f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915e6b65c14e46094253e14697d93e98",
+    "url": "/Conversor-Binario-Decimal/index.html"
+  },
+  {
+    "revision": "f74bc8297245104f77b4",
+    "url": "/Conversor-Binario-Decimal/static/js/2.0350b810.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Conversor-Binario-Decimal/static/js/2.0350b810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "883fdb7e577d5d8b8f8b",
+    "url": "/Conversor-Binario-Decimal/static/js/main.5e08a6a3.chunk.js"
+  },
+  {
+    "revision": "69246809ed96442f6310",
+    "url": "/Conversor-Binario-Decimal/static/js/runtime-main.d1be3175.js"
+  }
+]);
